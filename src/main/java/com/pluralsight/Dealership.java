51f6b188bbf22888121a29155fe8a90257595616
notLogi <92322066+notLogi@users.noreply.github.com>
2025-10-23 13:43:31 -0400

    Finished Phase 1

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -17,25 +17,25 @@ public class Dealership {
     }
 
     public List<Vehicle> getVehiclesByPrice(double min, double max){
-
+        return null;
     }
     public List<Vehicle> getVehiclesByMakeModel(String make, String model){
-
+        return null;
     }
     public List<Vehicle> getVehiclesByYear(int min, int max){
-
+        return null;
     }
     public List<Vehicle> getVehiclesByColor(String color){
-
+        return null;
     }
     public List<Vehicle> getVehiclesByMileage(double min, double max){
-
+        return null;
     }
     public List<Vehicle> getVehiclesByType(String vehicleType){
-
+        return null;
     }
     public List<Vehicle> getAllVehicles(){
-
+        return null;
     }
     public void addVehicle(Vehicle vehicle){
 
